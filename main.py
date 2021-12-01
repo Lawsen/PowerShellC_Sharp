095be33c74b356850c06c4729d5c2ee2fed3690b
Lawsen <lancewhite@live.com>
2021-12-01 10:56:00 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import os
+
+try:
+  import numpy as np
+except ImportError:
+  print("Trying to Install required module: numpy\n")
+  os.system('python -m pip install numpy')
+  import numpy as np
+
 def print_hi():
     print(f'Hello World!!!!')
+    a = np.arange(15).reshape(3,5)
+    print(a)
 
 if __name__ == '__main__':
     print_hi()
